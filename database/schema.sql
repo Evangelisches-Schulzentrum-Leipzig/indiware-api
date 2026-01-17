dfac3e8a43d170659c2e4f84226c4951b3ee581d
Caspar Kroll <ckroll@schulzentrum.de>
2026-01-18 00:09:42 +0100

    removed version tag from schema db

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 -- MariaDB 12+
-CREATE DATABASE IF NOT EXISTS timetable_v3 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS timetable CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-USE timetable_v3;
+USE timetable;
 
 -- ==========================================
 -- 1. Base Data (teachers, classes, subjects, buildings, rooms, periods, weekType, weeks, holidays, planType, queryMetadata)
